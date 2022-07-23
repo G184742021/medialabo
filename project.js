4aa24b708dda6ec25737da0d7a6c6c7ac3030edd
G184742021 <218474@st.takushoku-u.ac.jp>
2022-07-23 18:01:38 +0900

    第六回

--- project.js
+++ project.js
@@ -1,5 +1,73 @@
+let a = document.querySelector('div#cairo');
+a.addEventListener('click', cairotenki);
+let b = document.querySelector('div#moscow');
+a.addEventListener('click', moscowtenki);
+let c = document.querySelector('div#johannesburg');
+a.addEventListener('click', johannesburgtenki);
+let d = document.querySelector('div#beijng');
+a.addEventListener('click', beijingtenki);
+let e = document.querySelector('div#tokyo');
+a.addEventListener('click', tokyotenki);
+let f = document.querySelector('div#singapore');
+a.addEventListener('click', singaporetenki);
+let g = document.querySelector('div#sydney');
+a.addEventListener('click', sydneytenki);
+let h = document.querySelector('div#london');
+a.addEventListener('click', londontenki);
+let i = document.querySelector('div#paris');
+a.addEventListener('click', paristenki);
+let j = document.querySelector('div#riodejaneiro');
+a.addEventListener('click', riodejaneirotenki);
+let k = document.querySelector('div#newyork');
+a.addEventListener('click', newyorktenki);
+let l = document.querySelector('div#losangeles');
+a.addEventListener('click', losangelestenki);
 
-
-////////// 課題3-2 ここからプログラムを書こう
-
-console.log(data.name + ", " + data.main.temp_max + ", " + data.main.temp_min);
+function cairotenki() {
+    let b = document.querySelector('div#cairo');
+    b.style.color = 'blue';
+}
+function moscowtenki() {
+    let b = document.querySelector('div#moscow');
+    b.style.color = 'blue';
+}
+function johannesburgtenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function beijingtenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function tokyotenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function singaporetenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function sydneytenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function londontenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function paristenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function riodejaneirotenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function newyorktenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
+function losangelestenki() {
+    let b = document.querySelector('li');
+    b.style.color = 'blue';
+}
